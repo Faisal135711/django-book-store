84c3cf6d72dcf6443eba8b2fc79c02ea0da1e1eb
Faisal135711 <alpha316mnr@gmail.com>
2022-08-31 21:27:15 +0600

    implement homepage views

--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from store.models import Category, Book 
+
 # Create your views here.
+def all_books(request):
+    books = Book.objects.all()
+    return render(request, 'store/home.html', {'books': books})
