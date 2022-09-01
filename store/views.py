d0a0f2048b251bc420e365fb957d8add535b1e67
Faisal135711 <alpha316mnr@gmail.com>
2022-09-01 21:29:55 +0600

    make categories available in all templates
    By adding categories views from store.views to settings TEMPLATES
    OPTIONS, categories returned by that views will be available in all
    templates

--- store/views.py
+++ store/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 from store.models import Category, Book 
 
-# Create your views here.
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+
 def all_books(request):
     books = Book.objects.all()
     return render(request, 'store/home.html', {'books': books})
