e3373eb11c8faba66f7c54b5f40465ab492f77d3
Faisal135711 <alpha316mnr@gmail.com>
2022-09-03 21:44:03 +0600

    add default image for book

--- store/models.py
+++ store/models.py
@@ -28,7 +28,7 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, default='admin')
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', default='images/default.png')
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     in_stock = models.BooleanField(default=True)
