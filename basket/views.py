4de4fb04ad3842f6c403d16d3ab6bf64a2c53c93
Faisal135711 <alpha316mnr@gmail.com>
2022-09-11 17:41:29 +0600

    show total basket count in navbar

--- basket/views.py
+++ basket/views.py
@@ -17,5 +17,7 @@ def basket_add(request):
         book_qty = int(request.POST.get('bookQty'))
         book = get_object_or_404(Book, id=book_id)
         basket.add(book=book, qty=book_qty)
-        response = JsonResponse({'qty': book_qty})
+
+        basketQty = basket.__len__()
+        response = JsonResponse({'qty': basketQty})
         return response
