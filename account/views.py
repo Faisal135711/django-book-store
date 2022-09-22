ee1187cd26ddc8343ec29569be0c3b04a4ed059c
Faisal135711 <alpha316mnr@gmail.com>
2022-09-22 22:04:12 +0600

    define user activation view

--- account/views.py
+++ account/views.py
@@ -41,3 +41,19 @@ def account_register(request):
     return render(request, 'account/registration/register.html', {'form': registerForm})
 
 
+def account_activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = UserBase.objects.get(pk=uid)
+    except:
+        pass
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('account:dashboard')
+    else:
+        return render(request, 'account/registration/activation_invalid.html')
+
+
