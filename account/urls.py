72c00d62fe779f9960d712f4f8d2e986a990401e
Faisal135711 <alpha316mnr@gmail.com>
2022-09-24 15:55:14 +0600

    define user logout url

--- account/urls.py
+++ account/urls.py
@@ -9,6 +9,7 @@ app_name = 'account'
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name='account/registration/login.html',
                                                 form_class=UserLoginForm), name='login'),
+    path("logout/", auth_views.LogoutView.as_view(next_page='accout/login'), name='logout'),
     path("register/", views.account_register, name='register'),
     path("activate/<slug:uidb64>/<slug:token>/", views.account_activate, name='activate'),
     path("dashboard", views.dashboard, name='dashboard'),
