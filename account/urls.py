e3973ad3d40ce58605c43a9d0c2da87fd4f94431
Faisal135711 <alpha316mnr@gmail.com>
2022-09-24 15:54:51 +0600

    define user login url

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from account import views
+from account.forms import UserLoginForm
 
 app_name = 'account'
 
 urlpatterns = [
+    path("login/", auth_views.LoginView.as_view(template_name='account/registration/login.html',
+                                                form_class=UserLoginForm), name='login'),
     path("register/", views.account_register, name='register'),
     path("activate/<slug:uidb64>/<slug:token>/", views.account_activate, name='activate'),
     path("dashboard", views.dashboard, name='dashboard'),
