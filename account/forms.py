cd18dbbdb6a4f9b756d1044bab3c36d2f67ed4fd
Faisal135711 <alpha316mnr@gmail.com>
2022-09-24 15:54:00 +0600

    create user login form

--- account/forms.py
+++ account/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 from account.models import UserBase
 
@@ -47,3 +48,12 @@ class RegistrationForm(forms.ModelForm):
             {'class': 'form-control mb-3', 'placeholder': 'Password'})
         self.fields['password2'].widget.attrs.update(
             {'class': 'form-control', 'placeholder': 'Confirm Password'})
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control mb-3',
+                                                             'placeholder': 'Username',
+                                                             'id': 'login-user'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                 'placeholder': 'Password',
+                                                                 'id': 'login-pwd'}))
